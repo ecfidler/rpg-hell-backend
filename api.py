8d920dd410b85dcf6359998b5df2295be2b97937
Ethan Fidler <ethancf1999@gmail.com>
2023-11-05 20:02:20 -0500

    formatting & making tiny changes

--- api.py
+++ api.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI, HTTPException, status, Response
 from fastapi.middleware.cors import CORSMiddleware
 
+from fastapi.responses import JSONResponse
+
+import bl
+
 tags = [
     {
+        "name": "Temporary",
+        "description": "A temporary endpoint that will not exist when this api has been depoloyed to production."
+    },
+    {
+        "name": "Objects",
+        "description": ""
+    },
+    {
         "name": "Traits",
-        "description": "Files stored in storage on the printer."
+        "description": ""
     },
     {
         "name": "Spells",
-        "description": "Physical data collected by the printer."
+        "description": ""
     },
     {
         "name": "Items",
-        "description": "Control for the printing processes."
+        "description": ""
     }
 ]
 
@@ -33,3 +45,79 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+
+
+@app.put("/openquery", tags=["Temporary"])
+async def open_query(query: str):
+    """Takes in a raw SQL query. """
+    return JSONResponse(content={"data": bl.open_query(query)}, status_code=status.HTTP_200_OK)
+
+# Object CRUD methods
+
+
+# api.com/object/?name='something'
+@app.get("/object/", tags=["Objects"])
+async def object_search(name: str):
+    '''This method will eventually support filtering all values. Not just name.'''
+    return JSONResponse(content={"data": bl.object_search(name)}, status_code=status.HTTP_200_OK)
+
+
+# api.com/object/5
+@app.get("/object/{id}", tags=["Objects"])
+async def get_object_by_id(id: int):
+    return JSONResponse(content={"data": bl.get_object(id)}, status_code=status.HTTP_200_OK)
+
+
+@app.put("/item/", tags=["Items"])
+async def put_item(name: str, effect: str, cost: int, craft: int, tags: str, req: str = ""):
+    res = bl.create_item(name, effect, cost, craft, tags, req)
+    if (res == -1):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"it failed and we don't have proper error catching yet 😨")
+    else:
+        return JSONResponse(content={"data": res}, status_code=status.HTTP_200_OK)
+
+
+@app.put("/trait/", tags=["Traits"])
+async def put_trait(name: str, effect: str, req: str, dice: int, is_passive: bool):
+    res = bl.create_trait(name, effect, req, dice, is_passive)
+    if (res == -1):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"it failed and we don't have proper error catching yet 😨")
+    else:
+        return JSONResponse(content={"data": res}, status_code=status.HTTP_200_OK)
+
+# Spell CRUD methods
+
+
+@app.get("/spell/", tags=["Spells"])
+async def spell_search(name: str):
+    '''This method will eventually support filtering all values. Not just name.'''
+    return JSONResponse(content={"data": bl.spell_search(name)}, status_code=status.HTTP_200_OK)
+
+
+@app.get("/spell/{id}", tags=["Spells"])
+async def get_spell_by_id(id: int):
+    return JSONResponse(content={"data": bl.get_spell(int)}, status_code=status.HTTP_200_OK)
+
+
+@app.put("/spell/", tags=["Spells"])
+async def put_spell(name: str, effect: str, dice: int, level: int, tags: str):
+    res = bl.create_spell(name, effect, dice, level, tags)
+    if (res == -1):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"it failed and we don't have proper error catching yet 😨")
+    else:
+        return JSONResponse(content={"data": res}, status_code=status.HTTP_200_OK)
+
+'''
+
+get all formal route
+update item
+update trait
+update spell
+robust "search" functionality for spells & objects
+delete object 
+delete spell
+
+'''
--- csv_to_db.py
+++ csv_to_db.py
@@ -1,58 +1,54 @@
 from translator import *
 import csv
 
-def spliter(data):
-    lst = str(data).lower().split(", ")
-    return(lst)
-
 
 def clean_csv(csv_path):
-    with open(csv_path,'r') as f:
+    with open(csv_path, 'r') as f:
         clean = ''.join([i if ord(i) < 128 else ' ' for i in f.read()])
 
-    with open(csv_path.replace(".csv","_clean.csv"),'w') as f:
+    with open(csv_path.replace(".csv", "_clean.csv"), 'w') as f:
         f.write(clean)
 
+
 def get_dice(data):
-    data = data.replace("Attack","##").replace("Contest","##").replace("11","##").replace("+","")
-    
+    data = data.replace("Attack", "##").replace(
+        "Contest", "##").replace("11", "##").replace("+", "")
+
     dice = data.count("#")
     if "P" in data:
-        return(dice*-1)
-    return(dice)
+        return (dice*-1)
+    return (dice)
+
 
 def get_cost(data):
     d = data.split(", ")
-    lvl = int(d[1].replace("St ",""))
-    return get_dice(d[0]),lvl
-
+    lvl = int(d[1].replace("St ", ""))
+    return get_dice(d[0]), lvl
 
 
 def create_from_csv(csv_path):
     with open(csv_path, encoding="utf8", newline='') as f:
         reader = list(csv.reader(f))
-        
 
-        typ = 0 # defaults to spells
-        if 'Dice Cost' in reader[0]: # check for trait
-            typ = 1 
-        elif "Defence" in reader[0]: # check for armor
+        typ = 0  # defaults to spells
+        if 'Dice Cost' in reader[0]:  # check for trait
+            typ = 1
+        elif "Defence" in reader[0]:  # check for armor
             typ = 2
-        elif "Fists" in reader[1]: # check for weapons
+        elif "Fists" in reader[1]:  # check for weapons
             typ = 3
-        elif "Craft" in reader[0]: # check for items
+        elif "Craft" in reader[0]:  # check for items
             typ = 4
 
-
-        for row in list(reader)[1:]: # skip the header
+        for row in list(reader)[1:]:  # skip the header
             obj = None
-            
+
             if typ == 0:
                 # Name,Cost,Effect,Tags
                 print(f"Making Spell {row[0]}")
                 dice, level = get_cost(row[1])
                 tags = spliter(row[3])
-                obj = Spell(row[0],row[2],dice,level,tags)
+                obj = Spell(row[0], row[2], dice, level, tags)
             elif typ == 1:
                 # Name,Requirements,Dice Cost,Effect
                 print(f"Making Trait {row[0]}")
@@ -62,30 +58,29 @@ def create_from_csv(csv_path):
                 if dice < 0:
                     dice = dice*-1
                     is_p = True
-                obj = Trait(row[0],row[3],req,dice,is_p)
+                obj = Trait(row[0], row[3], req, dice, is_p)
             elif typ == 2:
                 # Name,Requirement,Effect,Defence,Traits
                 print(f"Making Armor {row[0]}")
                 req = spliter(row[1])
                 tags = spliter(row[4])
-                obj = Item(row[0],"",0,0,tags,req)
+                obj = Item(row[0], "", 0, 0, tags, req)
             elif typ == 3:
                 # Name,Requirement,Tags,Effect
                 print(f"Making Weapon {row[0]}")
                 req = spliter(row[1])
                 tags = spliter(row[2])
-                obj = Item(row[0],row[3],0,0,tags,req)
+                obj = Item(row[0], row[3], 0, 0, tags, req)
             else:
                 # Name,Tags,Effect,Cost,Crafting
                 print(f"Making Item {row[0]}")
                 tags = spliter(row[1])
-                obj = Item(row[0],row[2],0,0,tags)
-            
+                obj = Item(row[0], row[2], 0, 0, tags)
+
             # print(obj.return_data())
-            
+
             create(obj)
             # print(f"Successfuly made {row[0]}")
-        
 
 
 if __name__ == "__main__":
@@ -94,7 +89,6 @@ if __name__ == "__main__":
     # create_from_csv("data/Weapons_all1104.csv")
 
     # create_from_csv("data/Traits_all1105.csv")
-    
 
     # MAGIC ERROR
     # clean_csv("data/Items_all.csv")
