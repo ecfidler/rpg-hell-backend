0c1d3fa1c377a092085c2262b37ced1d3c22f50f
Ethan Fidler <ethancf1999@gmail.com>
2023-11-08 17:20:06 -0500

    renaming "business logic" to "CRUD"

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi.responses import JSONResponse
 
-import bl
+import crud
 
 tags = [
     {
@@ -53,24 +53,26 @@ async def root():
 
 @app.put("/openquery", tags=["Temporary"])
 async def open_query(query: str):
-    """Takes in a raw SQL query. """
-    return JSONResponse(content={"data": bl.open_query(query)}, status_code=status.HTTP_200_OK)
+    """Takes in a raw SQL query."""
+    return JSONResponse(content={"data": crud.open_query(query)}, status_code=status.HTTP_200_OK)
 
 #######################################################################
 ######################## Object CRUD methods ##########################
 #######################################################################
 
-# api.com/object/?name='something'
+# api.example.com/object/?name='something'
+
+
 @app.get("/object/", tags=["Objects"])
 async def object_search(name: str):
     '''This method will eventually support filtering all values. Not just name.'''
-    return JSONResponse(content={"data": bl.object_search(name)}, status_code=status.HTTP_200_OK)
+    return JSONResponse(content={"data": crud.object_search(name)}, status_code=status.HTTP_200_OK)
 
 
-# api.com/object/5
+# api.example.com/object/5
 @app.get("/object/{id}", tags=["Objects"])
 async def get_object_by_id(id: int):
-    return JSONResponse(content={"data": bl.get_object(id)}, status_code=status.HTTP_200_OK)
+    return JSONResponse(content={"data": crud.get_object(id)}, status_code=status.HTTP_200_OK)
 
 #######################################################################
 ########################  Item CRUD methods  ##########################
@@ -79,7 +81,7 @@ async def get_object_by_id(id: int):
 
 @app.put("/item/", tags=["Items"])
 async def put_item(name: str, effect: str, cost: int, craft: int, tags: str, req: str = ""):
-    res = bl.create_item(name, effect, cost, craft, tags, req)
+    res = crud.create_item(name, effect, cost, craft, tags, req)
     if (res == -1):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"it failed and we don't have proper error catching yet 😨")
@@ -94,7 +96,7 @@ async def put_item(name: str, effect: str, cost: int, craft: int, tags: str, req
 
 @app.put("/trait/", tags=["Traits"])
 async def put_trait(name: str, effect: str, req: str, dice: int, is_passive: bool):
-    res = bl.create_trait(name, effect, req, dice, is_passive)
+    res = crud.create_trait(name, effect, req, dice, is_passive)
     if (res == -1):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"it failed and we don't have proper error catching yet 😨")
@@ -110,17 +112,22 @@ async def put_trait(name: str, effect: str, req: str, dice: int, is_passive: boo
 @app.get("/spell/", tags=["Spells"])
 async def spell_search(name: str):
     '''This method will eventually support filtering all values. Not just name.'''
-    return JSONResponse(content={"data": bl.spell_search(name)}, status_code=status.HTTP_200_OK)
+    return JSONResponse(content={"data": crud.spell_search(name)}, status_code=status.HTTP_200_OK)
+
+
+# @app.get("/spells/", tags=["Spells"])
+# async def get_spells():
+#     return JSONResponse(content={"data": crud.get_spells()}, status_code=status.HTTP_200_OK)
 
 
 @app.get("/spell/{id}", tags=["Spells"])
 async def get_spell_by_id(id: int):
-    return JSONResponse(content={"data": bl.get_spell(int)}, status_code=status.HTTP_200_OK)
+    return JSONResponse(content={"data": crud.get_spell(int)}, status_code=status.HTTP_200_OK)
 
 
 @app.put("/spell/", tags=["Spells"])
 async def put_spell(name: str, effect: str, dice: int, level: int, tags: str):
-    res = bl.create_spell(name, effect, dice, level, tags)
+    res = crud.create_spell(name, effect, dice, level, tags)
     if (res == -1):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"it failed and we don't have proper error catching yet 😨")
